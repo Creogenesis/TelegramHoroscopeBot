8ea76839a858a6d2fd04a462b2a2bd39b0b257f5
belk1ng <dmitrit.belkin.2001@mail.ru>
2021-01-07 17:40:23 +0300

    added parse horoscopes from a site

--- main.py
+++ main.py
@@ -1,40 +1,11 @@
 import telebot
 from telebot import types
 from private import TOKEN  # Импорт токена
-from random import choice
+import requests  # Импорт модулей для парсинга
+from bs4 import BeautifulSoup
+
 
 bot = telebot.TeleBot(TOKEN)
-
-first = [
-    "Сегодня — идеальный день для новых начинаний.",
-    "Оптимальный день для того, чтобы решиться на смелый поступок!",
-    "Будьте осторожны, сегодня звёзды могут повлиять на ваше финансовое состояние.",
-    "Лучшее время для того, чтобы начать новые отношения или разобраться со старыми.",
-    "Плодотворный день для того, чтобы разобраться с накопившимися делами."
-]
-
-second = [
-    "Но помните, что даже в этом случае нужно не забывать про", "Если поедете за город, заранее подумайте про",
-    "Те, кто сегодня нацелен выполнить множество дел, должны помнить про",
-    "Если у вас упадок сил, обратите внимание на",
-    "Помните, что мысли материальны, а значит вам в течение дня нужно постоянно думать про"
-]
-
-second_add = [
-    "отношения с друзьями и близкими.",
-    "работу и деловые вопросы, которые могут так некстати помешать планам.",
-    "себя и своё здоровье, иначе к вечеру возможен полный раздрай.",
-    "бытовые вопросы — особенно те, которые вы не доделали вчера.",
-    "отдых, чтобы не превратить себя в загнанную лошадь в конце месяца."
-]
-
-third = [
-    "Злые языки могут говорить вам обратное, но сегодня их слушать не нужно.",
-    "Знайте, что успех благоволит только настойчивым, поэтому посвятите этот день воспитанию духа.",
-    "Даже если вы не сможете уменьшить влияние ретроградного Меркурия, то хотя бы доведите дела до конца.",
-    "Не нужно бояться одиноких встреч — сегодня то самое время, когда они значат многое.",
-    "Если встретите незнакомца на пути — проявите участие, и тогда эта встреча посулит вам приятные хлопоты."
-]
 
 
 @bot.message_handler(content_types=['text'])  # Обработка текстовых сообщений
@@ -44,29 +15,29 @@ def get_text_messages(message):
                          'Привет, сейчас покажу тебе гороскоп на сегодня!')
 
         keyboard = types.InlineKeyboardMarkup()  # Готовим кнопки
-        aries_key = types.InlineKeyboardButton(text='Овен', callback_data='zodiac')
+        aries_key = types.InlineKeyboardButton(text='Овен', callback_data=1)
         keyboard.add(aries_key)
-        taurus_key = types.InlineKeyboardButton(text='Телец', callback_data='zodiac')
+        taurus_key = types.InlineKeyboardButton(text='Телец', callback_data=2)
         keyboard.add(taurus_key)
-        gemini_key = types.InlineKeyboardButton(text='Близнецы', callback_data='zodiac')
+        gemini_key = types.InlineKeyboardButton(text='Близнецы', callback_data=3)
         keyboard.add(gemini_key)
-        cancer_key = types.InlineKeyboardButton(text='Рак', callback_data='zodiac')
+        cancer_key = types.InlineKeyboardButton(text='Рак', callback_data=4)
         keyboard.add(cancer_key)
-        leo_key = types.InlineKeyboardButton(text='Лев', callback_data='zodiac')
+        leo_key = types.InlineKeyboardButton(text='Лев', callback_data=5)
         keyboard.add(leo_key)
-        virgo_key = types.InlineKeyboardButton(text='Дева', callback_data='zodiac')
+        virgo_key = types.InlineKeyboardButton(text='Дева', callback_data=6)
         keyboard.add(virgo_key)
-        libra_key = types.InlineKeyboardButton(text='Весы', callback_data='zodiac')
+        libra_key = types.InlineKeyboardButton(text='Весы', callback_data=7)
         keyboard.add(libra_key)
-        scorpio_key = types.InlineKeyboardButton(text='Скорпион', callback_data='zodiac')
+        scorpio_key = types.InlineKeyboardButton(text='Скорпион', callback_data=8)
         keyboard.add(scorpio_key)
-        sagittarius_key = types.InlineKeyboardButton(text='Стрелец', callback_data='zodiac')
+        sagittarius_key = types.InlineKeyboardButton(text='Стрелец', callback_data=9)
         keyboard.add(sagittarius_key)
-        capricorn_key = types.InlineKeyboardButton(text='Козерог', callback_data='zodiac')
+        capricorn_key = types.InlineKeyboardButton(text='Козерог', callback_data=10)
         keyboard.add(capricorn_key)
-        aquarius_key = types.InlineKeyboardButton(text='Водолей', callback_data='zodiac')
+        aquarius_key = types.InlineKeyboardButton(text='Водолей', callback_data=11)
         keyboard.add(aquarius_key)
-        pisces_key = types.InlineKeyboardButton(text='Рыбы', callback_data='zodiac')
+        pisces_key = types.InlineKeyboardButton(text='Рыбы', callback_data=12)
         keyboard.add(pisces_key)
         bot.send_message(message.from_user.id,
                          text='Выбери свой знак зодиака', reply_markup=keyboard)  # Отображаем кноки
@@ -88,9 +59,30 @@ def get_text_messages(message):
 
 @bot.callback_query_handler(func=lambda call: True)  # Обработка нажатий на кнопки
 def callback(call):
-    if call.data == 'zodiac':
-        answer = f'{choice(first)} {choice(second)} {choice(second_add)} {choice(third)}'  # Формирование ответа
-        bot.send_message(call.message.chat.id, answer)
+    zodiacs = {
+        1: 'aries',
+        2: 'taurus',
+        3: 'gemini',
+        4: 'cancer',
+        5: 'leo',
+        6: 'virgo',
+        7: 'libra',
+        8: 'scorpio',
+        9: 'sagittarius',
+        10: 'capricorn',
+        11: 'aquarius',
+        12: 'pisces'
+    }
+
+    url = f'https://1001goroskop.ru/?znak={zodiacs[int(call.data)]}'  # URL адрес
+    answer = ''
+    request = requests.get(url)  # Отправлямем GET запрос
+    soup = BeautifulSoup(request.text, 'html.parser')
+    data = soup.find('p')  # Ищем обзац, где написан нужный текст (он там один, поэтому аргументов больше нет)
+    for string in data:
+        answer += string  # Формируем ответ
+    bot.send_message(call.message.chat.id, 'Гороскоп для выбранного знака на сегодня:')  # Добавить знак зодиака
+    bot.send_message(call.message.chat.id, answer)
 
 
 bot.polling(none_stop=True, interval=0)  # Непрерывная работа бота
